50656a3f549773e02ab15d0a319be32ca49bcafd
mshuaib777 <mshuaibgithub@gmail.com>
2026-08-04 09:40:01 +0530

    Adding Practical1_4

---
+++ Practical No. 1/practical1_4.js
@@ -0,0 +1,9 @@
+let firstName = "Mohammed Shuaib";
+let lastName = "Yelukar";
+
+// Function
+function greet(fname, lname) {
+    return "Hello " + fname + " " + lname;
+}
+
+console.log(greet(firstName, lastName));
