60e3996a9dbde97cf05225c87f81e02861b85ca5
mshuaib777 <mshuaibgithub@gmail.com>
2026-08-10 13:18:39 +0000

    MERN STACK PRACTICAL

---
+++ Practical No. 1/practical1_5.js
@@ -0,0 +1,14 @@
+let num1 = 75;
+let num2 = 60;
+
+// Operators
+console.log("Addition:", num1 + num2);
+console.log("Greater:", num1 > num2);
+console.log("Equal:", num1 == num2);
+
+// Control Statement
+if (num1 > num2) {
+    console.log("num1 is greater");
+} else {
+    console.log("num2 is greater");
+}
