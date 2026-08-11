5ddd48e5e547b5621ac22551620e4638c46727cb
mshuaib777 <mshuaibgithub@gmail.com>
2026-08-11 11:16:28 +0000

    Practical 3_1 completed

---
+++ Practical No. 3/react-project/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Welcome to MERN Stack Development</h1>
+      <p>Hello! I am a developer Mohammed Shuaib Yelukar</p>
+    </div>
+  );
+}
+
+export default App;
