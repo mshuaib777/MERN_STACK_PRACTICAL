9abc7ad58cca39e2aab864ed1e30fa76271c8e72
mshuaib777 <mshuaibgithub@gmail.com>
2026-08-12 07:37:35 +0000

    Added Practical 4_1

---
+++ Practical No. 4/Practical 4_1/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./Header";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <p>This is the main content.</p>
+      <p>I am developer Mohammed Shuaib</p>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
